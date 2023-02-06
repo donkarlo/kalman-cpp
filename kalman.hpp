96773264c4bf22648ce0571b12d0daa2991f2e5c
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2023-02-06 15:50:22 +0100

    reformatted

--- kalman.hpp
+++ kalman.hpp
@@ -6,77 +6,78 @@ class KalmanFilter {
 
 public:
 
-  /**
-  *   The constructor
-  *   processMatrix - System dynamics matrix
-  *   obsMtx - observation matrix
-  *   processNoiseCov - Process noise covariance
-  *   obsNoiseCov - Measurement noise covariance
-  *   estimatErrCov - Estimate error covariance
-  */
-  KalmanFilter(
-      double dt,
-      const Eigen::MatrixXd processMatrix,
-      const Eigen::MatrixXd obsMtx,
-      const Eigen::MatrixXd processNoiseCov,
-      const Eigen::MatrixXd obsNoiseCov,
-      const Eigen::MatrixXd estimatErrCov
-  );
+    /**
+    *   The constructor
+    *   processMatrix - System dynamics matrix
+    *   obsMtx - observation matrix
+    *   processNoiseCov - Process noise covariance
+    *   obsNoiseCov - Measurement noise covariance
+    *   estimatErrCov - Estimate error covariance
+    */
+    KalmanFilter(
+            double dt,
+            const Eigen::MatrixXd processMatrix,
+            const Eigen::MatrixXd obsMtx,
+            const Eigen::MatrixXd processNoiseCov,
+            const Eigen::MatrixXd obsNoiseCov,
+            const Eigen::MatrixXd estimatErrCov
+    );
 
-  /**
-  * Create a blank estimator.
-  */
-  KalmanFilter();
+    /**
+    * Create a blank estimator.
+    */
+    KalmanFilter();
 
-  /**
-  * Initialize the filter with initial states as zero.
-  */
-  void init();
+    /**
+    * Initialize the filter with initial states as zero.
+    */
+    void init();
 
-  /**
-  * Initialize the filter with a guess for initial states.
-  */
-  void init(double t0, const Eigen::VectorXd& x0);
+    /**
+    * Initialize the filter with a guess for initial states.
+    */
+    void init(double t0, const Eigen::VectorXd &x0);
 
-  /**
-  * Update the estimated state based on measured values. The
-  * time step is assumed to remain constant.
-  */
-  void update(const Eigen::VectorXd& obs);
+    /**
+    * Update the estimated state based on measured values. The
+    * time step is assumed to remain constant.
+    */
+    void update(const Eigen::VectorXd &obs);
 
-  /**
-  * Update the estimated state based on measured values,
-  * using the given time step and dynamics matrix.
-  */
-  void update(const Eigen::VectorXd& obs, double dt, const Eigen::MatrixXd processMtx);
+    /**
+    * Update the estimated state based on measured values,
+    * using the given time step and dynamics matrix.
+    */
+    void update(const Eigen::VectorXd &obs, double dt, const Eigen::MatrixXd processMtx);
 
-  /**
-  * Return the current state and time.
-  */
-  Eigen::VectorXd state() { return x_hat; };
-  double time() { return t; };
+    /**
+    * Return the current state and time.
+    */
+    Eigen::VectorXd state() { return x_hat; };
+
+    double time() { return t; };
 
 private:
 
-  // Matrices for computation
-  Eigen::MatrixXd processMtx, obsMtx, processNoiseCov, obsNoiseCov, estimatErrCov, kalmanGain, initEstimatedErrCov;
+    // Matrices for computation
+    Eigen::MatrixXd processMtx, obsMtx, processNoiseCov, obsNoiseCov, estimatErrCov, kalmanGain, initEstimatedErrCov;
 
-  // System dimensions
-  int stateDim, obsDim;
+    // System dimensions
+    int stateDim, obsDim;
 
-  // Initial and current time
-  double t0, t;
+    // Initial and current time
+    double t0, t;
 
-  // Discrete time step
-  double dt;
+    // Discrete time step
+    double dt;
 
-  // Is the filter initialized?
-  bool initialized;
+    // Is the filter initialized?
+    bool initialized;
 
-  // obsDim-size identity
-  Eigen::MatrixXd identityMtx;
+    // obsDim-size identity
+    Eigen::MatrixXd identityMtx;
 
-  // Estimated states
-  Eigen::VectorXd x_hat, x_hat_new;
+    // Estimated states
+    Eigen::VectorXd x_hat, x_hat_new;
 
 };
